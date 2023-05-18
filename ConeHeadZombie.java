16d8e4a6f1acae442f54213b9eb0ad4c572731a7
dsphanduc <phanduc.ds@gmail.com>
2023-05-18 20:42:34 +0700

    upload 2 kind of zombie

---
+++ ConeHeadZombie.java
@@ -0,0 +1,7 @@
+public class ConeHeadZombie extends Zombie {
+
+    public ConeHeadZombie(GamePanel parent,int lane){
+        super(parent,lane);
+        health = 1800;
+    }
+}
---
+++ NormalZombie.java
@@ -0,0 +1,7 @@
+public class NormalZombie extends Zombie {
+
+    public NormalZombie(GamePanel parent,int lane){
+        super(parent,lane);
+    }
+
+}
