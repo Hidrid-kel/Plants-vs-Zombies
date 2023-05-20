857ea4d84d4845787e01a6f07155a1fca4a6f374
Hidrid-kel <106839351+Hidrid-kel@users.noreply.github.com>
2023-05-20 19:11:45 +0700

    Add files via upload

---
+++ Plant.java
@@ -0,0 +1,25 @@
+
+public abstract class Plant {
+
+    public int health = 200;
+
+    public int x;
+    public int y;
+
+    public GamePanel gp;
+
+
+    public Plant(GamePanel parent,int x,int y){
+        this.x = x;
+        this.y = y;
+        gp = parent;
+    }
+
+    public void stop(){}
+    
+    
+    public static void main(String[]args){}
+}
+
+
+
