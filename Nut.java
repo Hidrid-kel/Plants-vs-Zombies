5e68513221f0ac7db81dbc0ec43812b8f472bd19
Hidrid-kel <106839351+Hidrid-kel@users.noreply.github.com>
2023-05-30 14:47:45 +0700

    Add files via upload

---
+++ Nut.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+public class Nut extends Plant{
+    
+    public Nut(GamePanel parent, int x, int y) {
+        super(parent, x, y);
+        health = 2000;}
+    }
+        
